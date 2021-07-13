bc79dd8079d864f23a5d564df91dd4868199a924
= <=>
2021-07-13 14:18:33 +0300

    add validation to form

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -7,3 +28,15 @@ function scrollFunction() {
     document.getElementById("navbar").classList.remove("noTransparrent")
   }
 }
+
+window.onload = function() {
+  var website = new CountUp("website-count", 0, 250, 0, 2.5);
+  website.start();
+  var app = new CountUp("app-count", 0, 100, 0, 2.5);
+  app.start();
+  var user = new CountUp("user-count", 0, 10000, 0, 2.5);
+  user.start();
+  var video = new CountUp("video-count", 0, 50, 0, 2.5);
+  video.start();
+}
+
